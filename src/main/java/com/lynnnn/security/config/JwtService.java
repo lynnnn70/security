bc3ff7b8ec7195ec394475156b09237bac0a96c4
lynnnn70 <lynnchiang1220@gmail.com>
2024-10-28 00:44:33 +0800

    add jwt service

--- src/main/java/com/lynnnn/security/config/JwtService.java
+++ src/main/java/com/lynnnn/security/config/JwtService.java
@@ -1,10 +1,39 @@
 package com.lynnnn.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTczMDAzNjM0MCwiaWF0IjoxNzMwMDM2MzQwfQ.UDhYD9DZz-SUk3_025lph7LqVxvK5ixM2Es-upVywuc";
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllCLaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllCLaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
